6a7fc50407291358bf117708a9a6c8c3669e0662
Maysafig <maysafig@gmail.com>
2022-07-22 04:59:45 -0300

    :sparkles: feat: conexao com o banco de dados

---
+++ src/app.js
@@ -0,0 +1,12 @@
+require("dotenv-safe").config()
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("./database/mongooseConnect")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect()
+
+module.exports = app 
