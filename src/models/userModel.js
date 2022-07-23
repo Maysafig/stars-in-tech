97b6746cd227bb185492b269b46abc47d80e217f
Maysafig <maysafig@gmail.com>
2022-07-22 21:14:27 -0300

    :sparkles: feat: Novo objeto no Schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,32 @@
 const moongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema({
-    nome: { type: String},
-    email: { type: String},
-    senha: { type: String}
-}, {
-    versionKey: false
-})
+const userSchema = moongoose.Schema({
+    _id: {
+        type: moongoose.Schema.Types.ObjectId,
+        default: moongoose.Types.ObjectId
+    },
+
+    nome: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    areaInteresse: {
+        type: String,
+        required: true
+    },
+
+    github: {
+        type: String,
+        default: "Não informado"
+    },
+
+    email: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
 
 const Model = mongoose.model("user", userSchema)
 
