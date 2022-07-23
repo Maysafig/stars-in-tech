b0656e889c8fefbe3a9247373883092d048acf8b
Maysafig <maysafig@gmail.com>
2022-07-23 11:36:05 -0300

    :sparkles: feat: Criacao da rota de index

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ require("dotenv-safe").config()
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("./database/mongooseConnect")
+const index = require("./routes/index")
 const userRoutes = require("./routes/userRoutes")
 const starsRoutes = require("./routes/starsRoutes")
+
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('../swagger/swagger_output.json')
 
@@ -11,10 +13,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use("/minha-rota-de-documentacao", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 mongoose.connect()
 
+app.use("/minha-rota-de-documentacao", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/", index)
 app.use(userRoutes)
 app.use(starsRoutes)
 
