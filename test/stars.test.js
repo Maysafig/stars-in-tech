0fc07b00e109e2e56904b5d6972986ca25256ba8
Maysafig <maysafig@gmail.com>
2022-07-23 15:27:50 -0300

    :test_tube: test: Criando novo teste para Model de Stars

---
+++ test/stars.test.js
@@ -0,0 +1,22 @@
+const Star = require("../src/models/starsModel")
+
+describe("Teste da model Stars", () => {
+    const stars = new Star({
+        id: 1,
+        nome: "Simara Conceição",
+        nomeUsuario: "quero ser dev",
+        instagram: "https://www.instagram.com/simara_conceicao",
+        youtube: "https://www.youtube.com/queroserdev",
+        linkedin: "https://www.linkedin.com/in/simaraconceicao/",
+        github: "https://github.com/simaraconceicao",
+        email: "contato@simaraconceicao.com"
+    })
+    it("Deve chamar o Schema e retornar um novo nome", () => {
+        expect(stars.nome).toBe("Simara Conceição")
+    })
+    it("Deve salvar no banco de dados o novo nome", () => {
+        stars.save().then((dados) => {
+            expect(dados.nome).toBe("Simara Conceição")
+        })
+    })
+})
