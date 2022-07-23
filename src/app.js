fb4bc474fa2f0c01f057be5fd0619690886ba59d
Maysafig <maysafig@gmail.com>
2022-07-22 22:21:49 -0300

    :sparkles: feat: Criacao das rotas de get e post das Users

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ require("dotenv-safe").config()
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("./database/mongooseConnect")
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect()
+
+app.use(userRoutes)
+app.use(starsRoutes)
 
 module.exports = app 
