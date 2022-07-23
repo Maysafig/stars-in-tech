0699d48b306c3d55c322da036391c56470961056
Maysafig <maysafig@gmail.com>
2022-07-23 11:45:04 -0300

    :boom: fix: Revertendo variaveis inutilizadas

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const index = require("./routes/index")
 const userRoutes = require("./routes/userRoutes")
 const starsRoutes = require("./routes/starsRoutes")
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('../swagger/swagger_output.json')
+const swaggerUi = require("swagger-ui-express")
+const swaggerFile = require("../swagger/swagger_output.json")
 
 const app = express()
 
@@ -17,8 +17,8 @@ app.use(cors())
 mongoose.connect()
 
 app.use("/minha-rota-de-documentacao", swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use("/", index)
-app.use("/", userRoutes)
-app.use("/",starsRoutes)
+app.use(index)
+app.use(userRoutes)
+app.use(starsRoutes)
 
 module.exports = app 
