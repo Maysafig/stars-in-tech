e8bd5553ceb17a21ca4e518e3815dcf1c8e64e7f
Maysafig <maysafig@gmail.com>
2022-07-25 09:34:41 -0300

    :sparkles: feat: Criação de rotas GET, PUT e DELETE das Stars

--- src/controller/starsController.js
+++ src/controller/starsController.js
@@ -9,7 +9,6 @@ const createStar = async (req, res) => {
         })
 
         const savedStar = await newStar.save()
-
         res.status(201).json(savedStar)
     } catch (error) {
         console.error(error)
@@ -27,7 +26,52 @@ const findAllStars = async (req, res) => {
     }
 }
 
+const findStarById = async (req, res) => {
+    try {
+        const findStar = await StarModel.findById(req.params.id)
+        res.status(200).json(findStar)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const updateStar = async (req, res) => {
+    try {
+        const { nome, nomeUsuario, instagram, youtube, linkedin, github, email } = req.body
+        await StarModel.findByIdAndUpdate(req.params.id, { nome, nomeUsuario, instagram, youtube, linkedin, github, email })
+
+        const updatedStar = await StarModel.findById(req.params.id)
+        res.status(200).json(updatedStar)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const deleteStar = async (req, res) => {
+    try {
+        const { id } = req.params
+        const findStar = await StarModel.findById(id)
+
+        if (findStar == null) {
+            return res.status(404).json({ message: `A star com o id ${id} não foi encontrada.`})
+        }
+
+        await findStar.remove()
+
+        res.status(200).json({ message: `A star ${findStar.nome} foi deletada com sucesso.`})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
     createStar,
-    findAllStars
+    findAllStars,
+    findStarById,
+    updateStar, 
+    deleteStar
 }
--- src/routes/starsRoutes.js
+++ src/routes/starsRoutes.js
@@ -3,7 +3,10 @@ const express = require("express")
 
 const router = express.Router()
 
-router.post("/stars", controller.createStar)
+router.post("/stars/create", controller.createStar)
 router.get("/stars", controller.findAllStars)
+router.get("/star/:id", controller.findStarById)
+router.put("/star/update/:id", controller.updateStar)
+router.delete("/star/delete/:id", controller.deleteStar)
 
 module.exports = router 
