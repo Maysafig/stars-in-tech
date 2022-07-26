95a5c2b0837288f7345af023fb3876a984df4812
Maysafig <maysafig@gmail.com>
2022-07-25 23:08:44 -0300

    :test_tube: test: Atualizando o teste da Schema das Stars

--- test/stars.test.js
+++ test/stars.test.js
@@ -20,4 +20,58 @@ describe("Teste da model Stars", () => {
             expect(dados.nome).toBe("Simara Conceição")
         })
     })
+    
+    it("Deve chamar o Schema e retornar um novo nomeUsuario", () => {
+        expect(stars.nomeUsuario).toBe("quero ser dev")
+    })
+    it("Deve salvar no banco de dados o novo nomeUsuario", () => {
+        stars.save().then((dados) => {
+            expect(dados.nomeUsuario).toBe("quero ser dev")
+        })
+    })
+
+    it("Deve chamar o Schema e retornar um novo link do instagram", () => {
+        expect(stars.instagram).toBe("https://www.instagram.com/simara_conceicao")
+    })
+    it("Deve salvar no banco de dados o novo link do instagram", () => {
+        stars.save().then((dados) => {
+            expect(dados.instagram).toBe("https://www.instagram.com/simara_conceicao")
+        })
+    })
+
+    it("Deve chamar o Schema e retornar um novo link do youtube", () => {
+        expect(stars.youtube).toBe("https://www.youtube.com/queroserdev")
+    })
+    it("Deve salvar no banco de dados o novo link do youtube", () => {
+        stars.save().then((dados) => {
+            expect(dados.youtube).toBe("https://www.youtube.com/queroserdev")
+        })
+    })
+
+    it("Deve chamar o Schema e retornar um novo link do linkedin", () => {
+        expect(stars.linkedin).toBe("https://www.linkedin.com/in/simaraconceicao/")
+    })
+    it("Deve salvar no banco de dados o novo link do linkedin", () => {
+        stars.save().then((dados) => {
+            expect(dados.linkedin).toBe("https://www.linkedin.com/in/simaraconceicao/")
+        })
+    })
+
+    it("Deve chamar o Schema e retornar um novo link do github", () => {
+        expect(stars.github).toBe("https://github.com/simaraconceicao")
+    })
+    it("Deve salvar no banco de dados o novo link do github", () => {
+        stars.save().then((dados) => {
+            expect(dados.github).toBe("https://github.com/simaraconceicao")
+        })
+    })
+
+    it("Deve chamar o Schema e retornar um novo email", () => {
+        expect(stars.email).toBe("contato@simaraconceicao.com")
+    })
+    it("Deve salvar no banco de dados o novo email", () => {
+        stars.save().then((dados) => {
+            expect(dados.email).toBe("contato@simaraconceicao.com")
+        })
+    })
 })
